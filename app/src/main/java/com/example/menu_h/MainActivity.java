f1ec8ffc8239e2ea1590efa00ecd912b4f8b5d3b
aayushkrr <66353335+aayushkrr@users.noreply.github.com>
2022-03-05 19:08:59 +0530

    nothing changed

--- app/src/main/java/com/example/menu_h/MainActivity.java
+++ app/src/main/java/com/example/menu_h/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // no code written here nothing done
 
     }
 
